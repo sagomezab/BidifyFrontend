c9227c4f06dd29fe3b7f3fa6994afa7aba495761
Santiago Gomez <sagomezab@gmail.com>
2023-11-23 17:16:37 -0500

    Ajustes al front de Subasta

--- src/app/Subasta/js/index.js
+++ src/app/Subasta/js/index.js
@@ -1,76 +1,73 @@
-const msgerForm = get(".msger-inputarea");
-const msgerInput = get(".msger-input");
-const msgerChat = get(".msger-chat");
+document.addEventListener('DOMContentLoaded', function () {
+  const msgerForm = document.getElementById("messageForm");
+  const msgerInput = document.getElementById("messageInput");
+  const msgerChat = document.querySelector(".msger-chat");
 
-const BOT_MSGS = [
-  "Hi, how are you?",
-  "Ohh... I can't understand what you trying to say. Sorry!",
-  "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
-  "I feel sleepy! :("
-];
+  const participants = ["Jaider", "Migue", "Santi", "Karen", "Sofia", "Majo"];
 
-// Icons made by Freepik from www.flaticon.com
-const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-const BOT_NAME = "BOT";
-const PERSON_NAME = "Sajad";
+  const participantsTableBody = $('#participantsTableBody');
+    participants.forEach(participant => {
+        participantsTableBody.append(`<tr><td>${participant}</td></tr>`);
+    });
 
-msgerForm.addEventListener("submit", event => {
-  event.preventDefault();
+  // Borra el contenido del chat al cargar la página
+  msgerChat.innerHTML = '';
 
-  const msgText = msgerInput.value;
-  if (!msgText) return;
+  msgerForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-  msgerInput.value = "";
+    const msgText = msgerInput.value.trim();
+    if (msgText !== '') {
+      appendMessage('right', 'You', msgText);
+      msgerInput.value = '';
+      // Comentar la siguiente línea para desactivar las respuestas automáticas del bot
+      // botResponse();
+    }
+  });
 
-  botResponse();
-});
-
-function appendMessage(name, img, side, text) {
-  //   Simple solution for small apps
-  const msgHTML = `
-    <div class="msg ${side}-msg">
-      <div class="msg-img" style="background-image: url(${img})"></div>
-
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">${name}</div>
-          <div class="msg-info-time">${formatDate(new Date())}</div>
+  function appendMessage(side, name, text) {
+    const msgHTML = `
+      <div class="msg ${side}-msg">
+        <div class="msg-bubble">
+          <div class="msg-info">
+            <div class="msg-info-name">${name}</div>
+            <div class="msg-info-time">${formatDate(new Date())}</div>
+          </div>
+          <div class="msg-text">${text}</div>
         </div>
-
-        <div class="msg-text">${text}</div>
       </div>
-    </div>
-  `;
+    `;
 
-  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-  msgerChat.scrollTop += 500;
-}
+    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat.scrollTop = msgerChat.scrollHeight;
+  }
 
-function botResponse() {
-  const r = random(0, BOT_MSGS.length - 1);
-  const msgText = BOT_MSGS[r];
-  const delay = msgText.split(" ").length * 100;
+  // Función para respuestas automáticas del bot
+  function botResponse() {
+    const BOT_MSGS = [
+      "Hi, how are you?",
+      "Ohh... I can't understand what you're trying to say. Sorry!",
+      "I like to play games... But I don't know how to play!",
+      "Sorry if my answers are not relevant. :))",
+      "I feel sleepy! :("
+    ];
 
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-  }, delay);
-}
+    const r = random(0, BOT_MSGS.length - 1);
+    const msgText = BOT_MSGS[r];
+    const delay = msgText.split(" ").length * 100;
 
-// Utils
-function get(selector, root = document) {
-  return root.querySelector(selector);
-}
+    setTimeout(() => {
+      appendMessage('left', 'BOT', msgText);
+    }, delay);
+  }
 
-function formatDate(date) {
-  const h = "0" + date.getHours();
-  const m = "0" + date.getMinutes();
+  function formatDate(date) {
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
+    return `${h.slice(-2)}:${m.slice(-2)}`;
+  }
 
-  return `${h.slice(-2)}:${m.slice(-2)}`;
-}
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+});
