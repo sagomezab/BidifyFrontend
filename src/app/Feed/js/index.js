18636cd32c431e52399353a88c33aba4026af961
Jaider Gonzalez <jaider.gonzalez@mail.escuelaing.edu.co>
2023-11-28 20:55:36 -0500

    Front lista subastas

--- src/app/Feed/js/index.js
+++ src/app/Feed/js/index.js
@@ -1,34 +1,29 @@
-$(document).ready(function() {
-    // Hacer solicitud GET al endpoint de subastas
-    $.ajax({
-        url: 'http://localhost:8080/subasta', // Reemplaza con la URL correcta de tu backend
-        type: 'GET',
-        dataType: 'json',
-        success: function(subastas) {
-            // Llenar la tabla con los datos de las subastas
-            llenarTabla(subastas);
-        },
-        error: function(error) {
-            console.log('Error al obtener subastas:', error);
-        }
-    });
-
-    // Función para llenar la tabla con los datos de las subastas
-    function llenarTabla(subastas) {
-        var tablaBody = $('.lista-subastas tbody');
-
-        // Limpiar contenido existente en la tabla
-        tablaBody.empty();
-
-        // Iterar sobre las subastas y agregar filas a la tabla
-        subastas.forEach(function(subasta, index) {
-            var fila = `<tr>
-                            <th scope="row">${index + 1}</th>
-                            <td>${subasta.subastador}</td>
-                            <td>${subasta.producto}</td>
-                            <td>${subasta.imagen}</td>
-                        </tr>`;
-            tablaBody.append(fila);
+function cargarSubastas() {
+    fetch('http://localhost:8080/subasta')
+      .then(response => response.json())
+      .then(subastas => {
+        
+        const tabla = document.getElementById('subastasTable');
+        const tbody = tabla.querySelector('tbody');
+  
+        
+        tbody.innerHTML = '';
+  
+        
+        subastas.forEach((subasta, index) => {
+          const fila = `
+            <tr>
+              <th scope="row">${index + 1}</th>
+              <td>${subasta.subastador.nombre}</td>
+              <td>${subasta.producto.nombre}</td>
+              <td><img src="${subasta.producto.img}" alt="Imagen de la subasta" class = "imagen-lista"></td>
+              <td><button type="button" class="btn btn-success">unirse</button></td>
+            </tr>
+          `;
+          tbody.innerHTML += fila;
         });
-    }
-});
+      })
+      .catch(error => console.error('Error al obtener las subastas:', error));
+  }
+  
+  cargarSubastas();
