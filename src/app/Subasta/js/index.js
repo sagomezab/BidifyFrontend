e18ba5d9e3684b99b90d71066058b42e16940bb7
Jaider Gonzalez <jaider.gonzalez@mail.escuelaing.edu.co>
2023-11-30 10:27:11 -0500

    Real-Time logrado

--- src/app/Subasta/js/index.js
+++ src/app/Subasta/js/index.js
@@ -1,33 +1,90 @@
-
 document.addEventListener('DOMContentLoaded', function () {
+  
   const subastaId = obtenerSubastaIdDesdeUrl();
   const socket = new SockJS('http://localhost:8080/stompendpoint');
   const stompClient = Stomp.over(socket);
-  stompClient.connect({}, function (frame) {
+  const userName = localStorage.getItem('userName');
+  mostrarMensajesEnInterfaz();
+
+  try {
+    stompClient.connect({}, function (frame) {
       console.log('Connected: ' + frame);
-      stompClient.subscribe('/topic/subasta/' + subastaId + '/actualizacion', function (subasta) {
-          const subastaActualizada = JSON.parse(subasta.body);
-          console.log('Subasta Actualizada:', subastaActualizada);
-          actualizarInterfazConSubasta(subastaActualizada);
+      
+      
+      stompClient.subscribe('/topic/subasta/' + subastaId + '/actualizacion', function (message) {
+        const subastaActualizada = JSON.parse(message.body);
+        console.log('Subasta Actualizada:', subastaActualizada);
+        actualizarInterfazConSubasta(subastaActualizada);
       });
-  });
+
+      stompClient.subscribe('/topic/subasta/' + subastaId + '/messages', function (message) {
+        try {
+          const response = JSON.parse(message.body);
+          mostrarMensajesEnInterfaz();
+          console.log('Mensaje añadido', response);
+        } catch (error) {
+          console.error('Error al analizar el mensaje JSON:', error);
+        }
+      });
+
+      stompClient.subscribe('/topic/subasta/' + subastaId + '/finalizar', function (message) {
+        const subastaFinalizada = JSON.parse(message.body);
+        console.log('Subasta Finalizada:', subastaFinalizada);
+        // Agregar lógica para manejar la finalización de la subasta en la interfaz
+      });
+    });
+
+    // Obtener la información inicial de la subasta
+    fetch(`http://localhost:8080/subasta/${subastaId}`)
+      .then(response => response.json())
+      .then(subasta => {
+        console.log('Subasta obtenida:', subasta);
+        actualizarInterfazConSubasta(subasta);
+      })
+      .catch(error => console.error('Error al obtener la subasta:', error));
+
+  } catch (error) {
+    console.error('Error en el código:', error);
+  }
 
   const msgerForm = document.getElementById("messageForm");
   const msgerInput = document.getElementById("messageInput");
   const msgerChat = document.querySelector(".msger-chat");
 
   msgerForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-
-      const msgText = msgerInput.value.trim();
-      if (msgText !== '') {
-          appendMessage('right', 'You', msgText);
-          msgerInput.value = '';
-      }
+    event.preventDefault();
+    const msgText = msgerInput.value.trim();
+    
+    if (msgText !== '') {
+      // Aquí puedes enviar mensajes al servidor a través de WebSocket
+      stompClient.send('/app/' + subastaId + '/messages', {}, JSON.stringify({
+        senderEmail: userName,
+        replymessage: msgText
+      }));
+      
+      appendMessage('right', 'You', msgText);
+      msgerInput.value = '';
+    }
   });
 
+  function mostrarMensajesEnInterfaz() {
+    const msgerChat = document.querySelector(".msger-chat");
+    msgerChat.innerHTML = ''; // Limpiar el contenido actual
+    fetch(`http://localhost:8080/subasta/${subastaId}/messages`)
+      .then(response => response.json())
+      .then(messages => {
+        messages.forEach(msg => {
+          const side = msg.senderEmail === userName ? 'right' : 'left';
+          appendMessage(side, msg.senderEmail, msg.replymessage);
+        });
+      })
+      .catch(error => console.error('Error al obtener mensajes:', error));
+  
+    
+  }
+
   function appendMessage(side, name, text) {
-      const msgHTML = `
+    const msgHTML = `
       <div class="msg ${side}-msg">
         <div class="msg-bubble">
           <div class="msg-info">
@@ -39,43 +96,42 @@ document.addEventListener('DOMContentLoaded', function () {
       </div>
     `;
 
-      msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-      msgerChat.scrollTop = msgerChat.scrollHeight;
+    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat.scrollTop = msgerChat.scrollHeight;
   }
 
   function obtenerSubastaIdDesdeUrl() {
-      
     const urlParams = new URLSearchParams(window.location.search);
     const subastaId = urlParams.get('id');
+    
     if (subastaId && !isNaN(subastaId)) {
-        return parseInt(subastaId);
+      return parseInt(subastaId);
     } else {
-        console.error("No se pudo obtener el ID de la subasta desde la URL.");
-        return null;
+      console.error("No se pudo obtener el ID de la subasta desde la URL.");
+      return null;
     }
-}
+  }
 
   function actualizarInterfazConSubasta(subasta) {
-      document.querySelector('.product-name').textContent = subasta.producto.nombre;
-      document.querySelector('.product-price').textContent = "Precio: " + subasta.precioFinal;
-      document.querySelector('.product-owner').textContent = "Dueño: " + subasta.subastador.nombre;
+    document.querySelector('.product-name').textContent = subasta.producto.nombre;
+    document.querySelector('.product-price').textContent = "Precio: " + subasta.precioFinal;
+    document.querySelector('.product-owner').textContent = "Dueño: " + subasta.subastador.nombre;
 
-      const productImage = document.querySelector('.product-image');
-      productImage.src = subasta.producto.img;
-      productImage.alt = "Imagen de la subasta";
+    const productImage = document.querySelector('.product-image');
+    productImage.src = subasta.producto.img;
+    productImage.alt = "Imagen de la subasta";
 
-      const participantsTableBody = document.getElementById('participantsTableBody');
-      participantsTableBody.innerHTML = '';
-      subasta.oferentes.forEach(participant => {
-          participantsTableBody.innerHTML += `<tr><td>${participant.nombre}</td></tr>`;
-      });
+    const participantsTableBody = document.getElementById('participantsTableBody');
+    participantsTableBody.innerHTML = '';
+
+    subasta.oferentes.forEach(participant => {
+      participantsTableBody.innerHTML += `<tr><td>${participant.nombre}</td></tr>`;
+    });
   }
 
   function formatDate(date) {
-      const h = "0" + date.getHours();
-      const m = "0" + date.getMinutes();
-      return `${h.slice(-2)}:${m.slice(-2)}`;
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
+    return `${h.slice(-2)}:${m.slice(-2)}`;
   }
-
-  
 });
