db7fa732aa10637edafca08547a53a4831da8af6
Miguel Angel Barrera Diaz <miguel.barrera-d@mail.escuelaing.edu.co>
2023-11-30 12:34:49 -0500

    Integración feed

--- src/app/Feed/js/index.js
+++ src/app/Feed/js/index.js
@@ -1,31 +1,31 @@
 function cargarSubastas() {
-    fetch('http://localhost:8080/subasta')
-      .then(response => response.json())
-      .then(subastas => {
-        
-        const tabla = document.getElementById('subastasTable');
-        const tbody = tabla.querySelector('tbody');
-  
-        
-        tbody.innerHTML = '';
-  
-        
-        subastas.forEach((subasta, index) => {
-          const fila = `
-            <tr>
-              <th scope="row">${index + 1}</th>
-              <td>${subasta.subastador.nombre}</td>
-              <td>${subasta.producto.nombre}</td>
-              <td><img src="${subasta.producto.img}" alt="Imagen de la subasta" class = "imagen-lista"></td>
-              <td><button type="button" class="btn btn-success" onclick="unirseASubasta(${subasta.id})">unirse</button></td>
-            </tr>
-          `;
-          tbody.innerHTML += fila;
-        });
-      })
-      .catch(error => console.error('Error al obtener las subastas:', error));
-  }
-  function unirseASubasta(subastaId) {
-    window.location.href = `../Subasta/index.html?id=${subastaId}`;
-  }
-  cargarSubastas();
+  fetch('http://localhost:8080/subasta')
+    .then(response => response.json())
+    .then(subastas => {
+      
+      const tabla = document.getElementById('subastasTable');
+      const tbody = tabla.querySelector('tbody');
+
+      
+      tbody.innerHTML = '';
+
+      
+      subastas.forEach((subasta, index) => {
+        const fila = `
+          <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${subasta.subastador.nombre}</td>
+            <td>${subasta.producto.nombre}</td>
+            <td><img src="${subasta.producto.img}" alt="Imagen de la subasta" class = "imagen-lista"></td>
+            <td><button type="button" class="button-33" onclick="unirseASubasta(${subasta.id})">unirse</button></td>
+          </tr>
+        `;
+        tbody.innerHTML += fila;
+      });
+    })
+    .catch(error => console.error('Error al obtener las subastas:', error));
+}
+function unirseASubasta(subastaId) {
+  window.location.href = `../Subasta/index.html?id=${subastaId}`;
+}
+cargarSubastas();
